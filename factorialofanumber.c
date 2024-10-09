f5e661d4a0eec696a5e2e59b7f4c62ca612dccc1
Adarsh Kumar Verma <109236222+aadr7h@users.noreply.github.com>
2024-10-09 22:18:51 +0530

    Add files via upload
    A quick way to calculate factorial using recursion

---
+++ factorialofanumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int factorial(int n) {
+	if ( n == 0) 
+	return 1;
+	return n * factorial(n - 1);
+}
+
+int main() {
+	int num = 5;
+	
+	printf("Factorial of %d is %d\n", num, factorial(num));
+	return 0;
+}
+
