a8ab97507efae571f112f58906da05dc3694de1b
Adarsh Kumar Verma <109236222+aadr7h@users.noreply.github.com>
2024-10-10 17:49:31 +0530

    Add files via upload

---
+++ sumofdigitsosanumber.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+	int num, sum = 0;
+	
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	
+	while (num != 0) {
+		sum += num % 10;
+		num /= 10;
+	}
+	
+	printf("Sum of digits: %d\n", sum);
+	return 0;
+}
